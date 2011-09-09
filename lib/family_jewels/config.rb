21715957225704d032d3536a9e82ced791947572
Jason Wadsworth <jdwadsworth@gmail.com>
2011-09-08 23:49:42 -0400

    Implemented fetching, merging and installing dependencies

--- lib/family_jewels/config.rb
+++ lib/family_jewels/config.rb
@@ -1,6 +1,12 @@
 module FamilyJewels
 	class Config
 		class << self
+			def gems_dir(create_if_missing = true)
+				dir = File.join(ENV['PWD'], 'gems')
+				FileUtils.mkdir(dir) if create_if_missing && !File.exists?(dir)
+				dir
+			end
+
 			def stage_dir(create_if_missing = true)
 				dir = File.join(ENV['PWD'], 'stage')
 				FileUtils.mkdir(dir) if create_if_missing && !File.exists?(dir)
--- lib/family_jewels/project.rb
+++ lib/family_jewels/project.rb
@@ -8,14 +8,16 @@ module FamilyJewels
 	class Project
 
 		DEFAULTS = {
+			:remote_name  => 'origin',
 			:branch_name  => 'master'
 		}
 
-		attr_accessor :clone_url, :branch_name, :project_name, :builder_name, :verbose
+		attr_accessor :clone_url, :branch_name, :project_name, :builder_name, :verbose, :remote_name
 
 		# Create a new Project.
 		#
 		# * :clone_url 		- The URL to use to clone the git repository
+		#   :remote_name  - The name of the git remote origin [default: origin]
 		#   :branch_name 	- The name of the git branch to build [default: master]
 		#   :project_name - The name of the project ( eg. srbase )
 		#   :builder_name - The name for this build of the project ( eg. srbase-master )
@@ -30,20 +32,10 @@ module FamilyJewels
 
 			self.verbose 		  = attributes[:verbose]
 			self.clone_url 		= attributes[:clone_url]
+			self.remote_name 	= attributes[:remote_name]
 			self.branch_name 	= attributes[:branch_name]
 			self.project_name = attributes[:project_name] || parse_project_name(clone_url)
 			self.builder_name = attributes[:builder_name] || "#{self.project_name}-#{self.branch_name}"
-
-			if self.verbose
-				puts
-				puts "Created Project:"
-				puts " - verbose      : #{self.verbose}"
-				puts " - clone_url    : #{self.clone_url}"
-				puts " - branch_name	: #{self.branch_name}"
-				puts " - project_name : #{self.project_name}"
-				puts " - builder_name : #{self.builder_name}"
-				puts
-			end
 		end
 
 		def clone_dir
@@ -57,23 +49,42 @@ module FamilyJewels
 
 		# Clones the project into the staging area
 		def clone!
-			run_command("git clone #{self.clone_url} #{clone_dir}")
+			puts "=> Cloning into #{self.clone_dir}" if self.verbose
+
+			run_command("git clone #{self.clone_url} #{self.clone_dir}")
+			FileUtils.cd(self.clone_dir) do
+				run_command("git checkout #{self.branch_name}")
+			end
 		end
 
 		# Fetches the latest changes from the remote repository.
 		# Returns true if the project has changed
 		def fetch!
-			false
+			puts "=> Fetching remote changes: #{self.remote_name}" if self.verbose
+
+			project_changed = false
+			FileUtils.cd(self.clone_dir) do
+				run_command("git fetch #{self.remote_name}")
+				run_command("git status") do |output|
+					project_changed = (output =~ /Your branch is behind/)
+				end
+			end
+			project_changed
 		end
 
 		# Resets the local branch to match the remote version.
-		def reset_branch!
+		def merge_branch!
+			puts "=> Resetting branch: #{self.branch_name}" if self.verbose
+
+			FileUtils.cd(self.clone_dir) do
+				run_command("git merge #{self.remote_name}/#{self.branch_name}")
+			end
 			false
 		end
 
 		# Returns true if the project has a Gemfile
 		def has_gemfile?
-			false
+			File.exists?(File.join(self.clone_dir, 'Gemfile'))
 		end
 
 		def has_install_task?
@@ -82,6 +93,12 @@ module FamilyJewels
 
 		# Processes the project, installing gems to be indexed as required.
 		def build(options = nil)
+
+			puts "\n======================================================================"
+			puts "=> Building: #{self.builder_name}"
+			self.print_attributes if self.verbose
+			puts "======================================================================\n\n"
+
 			options ||= {}
 			options = {
 				:install_dependencies => true,
@@ -90,28 +107,72 @@ module FamilyJewels
 
 			if self.cloned?
 				if self.fetch!
-					self.reset_branch!
+					self.merge_branch!
 				else
-					# No remote changes.
+					puts "\n======================================================================"
+					puts "=> No remote changes."
+					puts "======================================================================"
 					return false
 				end
 			else
 				self.clone!
 			end
 
-			run_command('bundle install') if self.has_gemfile? && option[:install_dependencies]
-			run_command('rake install') 	if self.has_install_task? && option[:install_as_gem]
+			if self.has_gemfile? && options[:install_dependencies]
+				puts "\n======================================================================"
+				puts "=> Installing Dependencies"
+				puts "======================================================================"
+
+				FileUtils.cd(self.clone_dir) do
+					run_command("bundle install --path #{Config.gems_dir}")
+				end
+			end
+
+			if self.has_install_task? && options[:install_as_gem]
+				puts "\n======================================================================"
+				puts "=> Installing Gem"
+				puts "======================================================================"
+
+				FileUtils.cd(self.clone_dir) do
+					# TODO: Change the environment so that the gem is intalled into Config.gems_dir
+					run_command('rake install')
+				end
+			end
 
 			return true
 		end
 
 		protected
 
-		def run_command(command)
-			puts " => Running: #{command}" if self.verbose
-			`#{command}`
+		def print_attributes
+			puts "=>  - verbose      : #{self.verbose}"
+			puts "=>  - clone_url    : #{self.clone_url}"
+			puts "=>  - remote_name	 : #{self.remote_name}"
+			puts "=>  - branch_name	 : #{self.branch_name}"
+			puts "=>  - project_name : #{self.project_name}"
+			puts "=>  - builder_name : #{self.builder_name}"
+		end
+
+		def parse_project_name(git_url)
+			git_url.match(/^.*\/(.*)\.git$/)[1]
+		end
+
+		def run_command(command, &block)
+			if self.verbose
+				puts "=> Running: #{command}"
+				puts
+			end
+
+			output = `#{command}`
 			successful = ($?.exitstatus == 0)
-			puts " => Success?: #{successful}" if self.verbose
+
+			if self.verbose
+				puts
+				puts "=> Success?: #{successful}"
+			end
+
+			yield(output) if block_given? && successful
+
 			return successful
 		end
 
